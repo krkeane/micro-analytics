6feab48fff694fc0dfec235e734defbbc6ad147d
Kevin R Keane <krkeane@buffalo.edu>
2021-07-10 13:51:44 -0400

    Add code.

--- r/R/app.R
+++ r/R/app.R
@@ -1,21 +1,38 @@
+#!/usr/bin/env Rscript
+
 library(RestRserve)
+
+calc_incr = function(n) {
+  n+1L
+}
+
+incr_handler = function(request, response) {
+  n = as.integer(request$parameters_query[["n"]])
+  if (length(n) == 0L || is.na(n)) {
+    raise(HTTPError$bad_request())
+  }
+  response$body = calc_incr(n)
+}
+
 app = Application$new()
 
 app$add_get(
-  path = "/health", 
-  FUN = function(.req, .res) {
-    .res$set_body("OK")
-  })
+  path = "/incr",
+  FUN = incr_handler
+)
 
-app$add_post(
-  path = "/addone", 
-  FUN = function(.req, .res) {
-    result = list(x = .req$body$x + 1L)
-    .res$set_content_type("application/json")
-    .res$set_body(result)
-  })
+app$add_openapi(
+  path = "/openapi.yaml",
+  file_path = "openapi.yaml"
+)
 
+app$add_swagger_ui(
+  path = "/swagger",
+  path_openapi = "/openapi.yaml",
+  path_swagger_assets = "/swagger/assets/",
+  file_path = tempfile(fileext = ".html"),
+  use_cdn = FALSE
+)
 
 backend = BackendRserve$new()
 backend$start(app, http_port = 8080)
-
