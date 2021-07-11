bc37fddbb448d0fc76df62a31e23395d0c7271ea
Kevin R Keane <krkeane@buffalo.edu>
2021-07-11 09:29:47 -0400

    Add Shiny example.

--- rshiny/R/app.R
+++ rshiny/R/app.R
@@ -1,49 +1,48 @@
-#!/usr/bin/env Rscript
-
 library(shiny)
 
-  readRenviron('/etc/os-release')
-  pn <- Sys.getenv("PRETTY_NAME")
+readRenviron('/etc/os-release')
+pn <- Sys.getenv("PRETTY_NAME")
 infoText <- sprintf(
-    "\n%s \"%s\"\n%s %s\n\n\t%d+1 == %d\n\n",
-    R.Version()$version.string,
-    R.Version()$nickname,
-    pn,
-    R.Version()$platform,
-    n,
-    calc_incr(n)
-  )
+  "\n%s \"%s\"\n%s %s\n\n",
+  R.Version()$version.string,
+  R.Version()$nickname,
+  pn,
+  R.Version()$platform
+)
 
 ui <- fluidPage(
   titlePanel("Hello Shiny!"),
   verbatimTextOutput(infoText),
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput(inputId = "bins",
-                  label = "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-
-    mainPanel(
-      plotOutput(outputId = "distPlot")
+  sidebarLayout(sidebarPanel(
+    sliderInput(
+      inputId = "bins",
+      label = "Number of bins:",
+      min = 1,
+      max = 50,
+      value = 30
     )
-  )
+  ),
+  
+  mainPanel(plotOutput(outputId = "distPlot")))
 )
 
 server <- function(input, output) {
   output$distPlot <- renderPlot({
     x    <- faithful$waiting
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    hist(x, breaks = bins, col = "#75AADB", border = "white",
-         xlab = "Waiting time to next eruption (in mins)",
-         main = "Histogram of waiting times")
-
-    })
-
+    
+    hist(
+      x,
+      breaks = bins,
+      col = "#75AADB",
+      border = "white",
+      xlab = "Waiting time to next eruption (in mins)",
+      main = "Histogram of waiting times"
+    )
+    
+  })
+  
 }
 
 # Create Shiny app ----
-shinyApp(ui = ui, server = server, port=8080)
+shinyApp(ui = ui, server = server)
---
+++ rshiny/R/shinyApp.R
@@ -0,0 +1,3 @@
+#!/usr/bin/env Rscript
+
+shiny::runApp('app.R',port=8080)
